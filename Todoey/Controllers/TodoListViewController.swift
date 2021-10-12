8a64267f16a0adba15e3468972a0ddf92d6ca4dc
Averiser <averiser@gmail.com>
2021-10-12 20:21:01 +0300

    tenth commit - Save Data with Core Data (Create in CRUD)

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,36 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
   
   var itemArray = [Item]()
-//    ["Find Mike", "Do Shadow Boxing", "Do Chin-ups", "Do push-ups", "Do warm-up", "Find Mike2", "Do Shadow Boxing 2", "Do Chin-ups2", "Do push-ups2", "Do warm-up2", "Find Mike3", "Do Shadow Boxing3", "Do Chin-ups3", "Do push-ups3", "Do warm-up3", "Find Mike4", "Do Shadow Boxing 4", "Do Chin-ups4", "Do push-ups4", "Do warm-up4"]
-  
   let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
       
       print(dataFilePath)
       
-//      let newItem = Item()
-//      newItem.title = "Find Mike"
-//      itemArray.append(newItem)
-//      
-//      let newItem2 = Item()
-//      newItem2.title = "Do Shadow Boxing"
-//      itemArray.append(newItem2)
-//      
-//      let newItem3 = Item()
-//      newItem3.title = "Do Chin-ups"
-//      itemArray.append(newItem3)
-      
 //      if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
 //        itemArray = items
 //      }
       
-      loadItems()
+//      loadItems()
     }
   
   //MARK - TableView Datasource methods
@@ -99,8 +87,9 @@ class TodoListViewController: UITableViewController {
     let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
       //what will happen once the user clicks the Add Item button on our UIAlert
       
-      let newItem = Item()
+      let newItem = Item(context: self.context)
       newItem.title = textField.text!
+      newItem.done = false
       
       self.itemArray.append(newItem)
       
@@ -121,28 +110,25 @@ class TodoListViewController: UITableViewController {
 // MARK - Model Manipulation Methods
   
   func saveItems() {
-    let encoder = PropertyListEncoder()
-    
     do {
-      let data = try encoder.encode(itemArray)
-      try data.write(to: dataFilePath!)
+      try context.save()
     } catch {
-      print("Error encoding item array, \(error)")
+      print("Error saving context \(error)")
     }
     
     tableView.reloadData()
   }
   
-  func loadItems() {
-    
-    if let data = try? Data(contentsOf: dataFilePath!) {
-      let decoder = PropertyListDecoder()
-      do {
-      itemArray = try decoder.decode([Item].self, from: data)
-      } catch {
-        print("Error decoding item array, \(error)")
-      }
-    }
-  }
+//  func loadItems() {
+//
+//    if let data = try? Data(contentsOf: dataFilePath!) {
+//      let decoder = PropertyListDecoder()
+//      do {
+//      itemArray = try decoder.decode([Item].self, from: data)
+//      } catch {
+//        print("Error decoding item array, \(error)")
+//      }
+//    }
+//  }
 }
 
