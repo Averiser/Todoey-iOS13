dc953670b2982615bea0afaeedb5091826716580
Averiser <averiser@gmail.com>
2021-10-27 23:15:42 +0300

    twenty-fifth  commit - 265 Removing Data from Realm (Delete in CRUD)

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -53,20 +53,14 @@ class TodoListViewController: UITableViewController {
     if let item = todoItems?[indexPath.row] {
       do {
         try realm.write {
-          item.done = !item.done
+          realm.delete(item)
+//          item.done = !item.done
         }
       } catch {
         print("Erro saving done status, \(error)")
       }
     }
     tableView.reloadData()
-    
-//    context.delete(itemArray[indexPath.row])
-//    itemArray.remove(at: indexPath.row)
-    
-//    todoItems[indexPath.row].done = !todoItems[indexPath.row].done
-//
-//    saveItems()
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
